eded9e78db6bddb95120e283abfef9c2e2cc37f3
kght6123 <i.feeling.song+github@gmail.com>
2019-05-30 07:02:09 +0900

    update GAE settings.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 export default {
 	path: '/api/index',
 	handler (req, res, next) {
-		console.time('index')
+		//console.time('index')
 
 		// req は Node.js の HTTPリクエストオブジェクトです
 		console.log('url=%s', req.url)
@@ -12,12 +12,12 @@ export default {
 		res.writeHead(200, { 'Content-Type': 'application/json' });
 		res.end(JSON.stringify({ 'foo': 'bar' }));
 		
-		console.trace('trace test : %o', req)
+		//console.trace('trace test : %o', req)
 
 		// next は 次のミドルウェアを呼び出すための関数です。
 		// あなたのミドルウェアが最後でない場合、関数の最後で next を呼び出すのを忘れないでください！
 		//next()
 
-		console.timeEnd('index')
+		//console.timeEnd('index')
 	}
 }
