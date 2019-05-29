b3c96f7933376087cb60ecfd8f480aa2a6bd6046
kght6123 <i.feeling.song+github@gmail.com>
2019-05-30 00:56:52 +0900

    stackdriver

--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
 export default {
 	path: '/api/index',
 	handler (req, res, next) {
+		console.time('index')
+
 		// req は Node.js の HTTPリクエストオブジェクトです
-		console.log(req.url)
+		console.log('url=%s', req.url)
+		console.error('error test : %j', next) // stderr に出力する。
 
 		// res は Node.js の HTTPレスポンスオブジェクトです
 
 		res.writeHead(200, { 'Content-Type': 'application/json' });
 		res.end(JSON.stringify({ 'foo': 'bar' }));
 		
+		console.trace('trace test : %o', req)
+
 		// next は 次のミドルウェアを呼び出すための関数です。
 		// あなたのミドルウェアが最後でない場合、関数の最後で next を呼び出すのを忘れないでください！
 		//next()
+
+		console.timeEnd('index')
 	}
 }
